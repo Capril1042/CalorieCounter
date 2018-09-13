89c45ed71a46fc0ac1aabc8fef0e8dc23fef2c49
Capril1042 <Caprilewis1042@gmail.com>
2018-09-13 17:56:58 -0400

    layout of landing page

--- track/src/Components/LandingPage.js
+++ track/src/Components/LandingPage.js
@@ -1,9 +1,24 @@
 import React,  { Component } from 'react';
+import '../styles.css'
 
 const LandingPage = (props) => {
     return(
-        <div>
-        Landing Page
+        <div className="landingpage">
+            <div className="landingnav">
+                <div className="logins">
+                <a href="/sign-in">Sign-In</a>
+                <a href="/sign-up"> Sign-Up</a>
+                </div>
+            </div>
+            <div className="welcome">
+                <h1>Welcome to Recipe Tracker</h1>
+                <div className="intro">
+                <p> Recipe tracker not only allows you to keep all your favorite
+                recipes in one place, it also allows you to find out the nutritional 
+                content of your favorite recipes. </p>
+                <p> Sign-up today to start keeping track of your favorite recipes.</p>
+            </div>
+            </div>
         </div>
     )
 }
