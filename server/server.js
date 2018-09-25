13574d052758dc2d493cc138ccf2a1fadfa04544
Capril1042 <Caprilewis1042@gmail.com>
2018-09-24 21:50:00 -0400

    server

--- server/server.js
+++ server/server.js
@@ -152,8 +152,8 @@ app.delete('/api/delete_recipe', (req, res)=>{
 
 if(process.env.NODE_ENV === 'production'){
     const path = require('path');
-    app.get('/*'),(req,res)=>{
-        res.sendfile(path.resolve(__dirname,'../client','build','index.html')
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
     })
 }
 
